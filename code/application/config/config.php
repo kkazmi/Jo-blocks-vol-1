661bd2df974428ef586833aa7d8b72f819c9b5c6
kkazmi <kkazmi@github.com>
2018-01-19 12:34:10 +0530

    add explorer

--- code/application/config/config.php
+++ code/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/joblocks.vol1/code/';
+$config['base_url'] = 'http://localhost/joblocks.vol1/Jo-blocks-vol-1/code/';
 
 /*
 |--------------------------------------------------------------------------
